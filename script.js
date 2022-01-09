98720fca738995492080821445f041cbcb46f168
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-01-09 15:36:33 +0200

    печать работает

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const elem = document.documentElement;
-let correctly;
 let FirstNumber = new Number();
 let LastNumber = new Number();
 let add;
@@ -15,6 +14,9 @@ let Examples = [];
 let Answers = [];
 let usersAnswers = [];
 let ExampleID = 0;
+
+let printCount = new Number();
+
 window.onload = function () {
     let data = new Date;
     let hours = data.getHours();
@@ -55,6 +57,8 @@ function FullScreen(element) {
         division = document.getElementById('division').checked;
         actionsCount = Number(document.getElementById('CountActionsInput').value);
         examplesCount = Number(document.getElementById('CountExamples').value);
+        printCount = Number(document.getElementById('versionCount').value);
+
         let j = 1;
         for (let i of [add, sub, multi, division]) {
             if (i == true) {
@@ -62,15 +66,19 @@ function FullScreen(element) {
             }
             j++
         }
-
-        if (element.requestFullscreen) {
-            element.requestFullscreen();
-        } else if (element.webkitrequestFullscreen) {
-            element.webkitRequestFullscreen();
-        }
         document.getElementById('container').style.display = 'none';
-        document.getElementById('case').style.display = 'flex'
-        ExamplesGeneration()
+        if (printCount != '') {
+            PrintCounts();
+        } else {
+            document.getElementById('case').style.display = 'flex';
+
+            if (element.requestFullscreen) {
+                element.requestFullscreen();
+            } else if (element.webkitrequestFullscreen) {
+                element.webkitRequestFullscreen();
+            }
+            ExamplesGeneration();
+        }
     }
 }
 //генерирование с проверкой корректности
@@ -87,7 +95,6 @@ function ChangeNumbersSize() {
     if (First == '' || Last == '' || First > Last) {
         document.getElementById('FirstNumber').classList.add('incorrectly');
         document.getElementById('LastNumber').classList.add('incorrectly');
-        // console.log('Диапазон неверный');
     }
     return (First <= Last);
 }
@@ -109,7 +116,6 @@ function ChangeActions() {
         for (let label of document.getElementById('ChangeActions').getElementsByTagName('label')) {
             label.classList.add('incorrectly')
         }
-        // console.log('Не выбраны действия')
     }
     return (!(sub == false && add == false && multi == false && division == false));
 }
@@ -124,7 +130,6 @@ function ActionsCount() {
 
     if (ActionsCount < 1 || ActionsCount != Math.trunc(ActionsCount) || ActionsCount == '') {
         document.getElementById('CountActionsInput').classList.add('incorrectly')
-        // console.log('Неверное количество действий')
     }
     return ((ActionsCount >= 1) && (ActionsCount == Math.trunc(ActionsCount)));
 }
@@ -137,23 +142,33 @@ function ExamplesCount() {
     document.getElementById('CountExamples').classList = '';
     if (count < 1 || Math.trunc(count) != count) {
         document.getElementById('CountExamples').classList.add('incorrectly')
-        // console.log('Неверное количество примеров')
     }
     return (count >= 1 && Math.trunc(count) == count);
 }
 // проверка количества примеров
 
 
-//таблица умножения
-function MultiTable() {
 
+// !Печать
+function PrintCounts() {
+    document.getElementById('body').style.background = 'white'
+    document.getElementById('body').style.width = 'auto';
+    document.getElementById('body').style.height = 'auto';
+    document.getElementById('containerToPrint').style.display = 'flex';
+    for (let i = 0; i < printCount; i++) {
+        document.getElementById('printExamples').innerHTML += "<div class='printBlocks'><h2>Вариант " + (i + 1) + "</h2></div>";
+        document.getElementById('printAnswers').innerHTML += "<div class='printBlocksAnswers'><h2>Вариант " + (i + 1) + "</h2></div>";
+        Answers = [];
+        Examples = [];
+        ExamplesGeneration();
+        for (let j = 0; j < Examples.length; j++) {
+            let LastPrintBlock = document.getElementsByClassName('printBlocks')[(document.getElementsByClassName('printBlocks').length) - 1];
+            let LastPrintBlockAnswers = document.getElementsByClassName('printBlocksAnswers')[(document.getElementsByClassName('printBlocksAnswers').length) - 1];
+            LastPrintBlock.innerHTML += '<div><p class="printExampleNumber">' + (j + 1) + ')</p><p class="printExample">' + (Examples[j]) + '=</p><div id="input"></div><div id="check"></div></div>';
+            LastPrintBlockAnswers.innerHTML += '<div><p class="printExampleNumber">' + (j + 1) + ') ' + (Answers[j]) + '</p></div>';
+        }
+    }
 }
-//таблица умножения
-
-
-
-
-
 
 
 // !Вывод примеров
@@ -166,8 +181,8 @@ function ExamplesStart() {
         document.getElementById('StudentName').innerText = studentName;
         document.getElementById('ExampleNumber').innerText = ExampleID + 1;
         document.getElementById('remainder').innerHTML = (examplesCount - 1);
-        
-        document.getElementById('realProgress').style.width =  (document.getElementById('progress').offsetWidth / examplesCount) + 'px';
+
+        document.getElementById('realProgress').style.width = (document.getElementById('progress').offsetWidth / examplesCount) + 'px';
         document.getElementById("Example").innerText = Examples[ExampleID] + ' =';
     }
 }
@@ -181,12 +196,11 @@ function NextExample() {
 
         document.getElementById('ExampleNumber').innerText = ExampleID + 1;
         document.getElementById('remainder').innerHTML = (examplesCount - (ExampleID + 1));
-        
+
         document.getElementById('Example').innerText = Examples[ExampleID] + ' =';
 
         document.getElementById('realProgress').style.width = document.getElementById('realProgress').offsetWidth + (document.getElementById('progress').offsetWidth / examplesCount) + 'px';
-    }
-    else if (document.getElementById('ExampleInput').value != '' && document.getElementById('remainder').innerText == 0) {
+    } else if (document.getElementById('ExampleInput').value != '' && document.getElementById('remainder').innerText == 0) {
         usersAnswers.push(document.getElementById('ExampleInput').value);
         document.getElementById('ExampleInput').value = '';
         AnswersView();
@@ -197,24 +211,21 @@ function AnswersView() {
     document.getElementById('ExamplesBlock').style.display = 'none';
     document.getElementById('AnswersBlock').style.display = 'flex';
     document.getElementById('StudentName2').innerText = studentName;
-    for (let i = 0; i < Examples.length; i++){
+    for (let i = 0; i < Examples.length; i++) {
         document.getElementById('AnswersBlock').innerHTML += '<div><p class="Number">' + (i + 1) + '</p><p class="ExamplesList">' + (Examples[i]) + '</p><p class="UserAnswer">' + (usersAnswers[i]) + '</p><p class="CorrectAnswer">' + (Answers[i]) + '</p></div>';
         if ((document.getElementsByClassName('UserAnswer')[(document.getElementsByClassName('UserAnswer').length) - 1].innerText) != (document.getElementsByClassName('CorrectAnswer')[(document.getElementsByClassName('CorrectAnswer').length) - 1].innerText)) {
             (document.getElementsByClassName('UserAnswer')[(document.getElementsByClassName('UserAnswer').length) - 1]).style.color = "#ff0000";
-        }
-        else {
+        } else {
             (document.getElementsByClassName('UserAnswer')[(document.getElementsByClassName('UserAnswer').length) - 1]).style.color = "#22f50f";
         }
-        document.getElementById('case').style.height = 'auto';
-        document.getElementById('case').style.margin = '40px 0';k
     }
+    document.getElementById('case').style.height = 'auto';
+    document.getElementById('case').style.margin = '40px 0';
 }
 
 // !Генерация примеров
 function ExamplesGeneration() {
     for (let i = 1; i <= examplesCount; i++) {
-        console.log('----')
-        console.log(Examples.length);
         document.getElementById('ExampleNumber').innerText = 1;
         let tempExample = '';
         let tempExampleMath = '';
@@ -227,10 +238,14 @@ function ExamplesGeneration() {
         for (let j = 1; j <= actionsCount; j++) {
             if (lastAction != 4) {
                 tempNumber = getRandomIntInclusive(FirstNumber, LastNumber);
+                let k = 0;
+                while (k <= 10 && tempNumber == 1) {
+                    tempNumber = getRandomIntInclusive(FirstNumber, LastNumber);
+                }
             }
             tempAction = Actions[getRandomIntInclusive(1, Actions.length) - 1];
             let k = 0;
-            while ((lastAction == tempAction  && k <= 10) || ((lastAction == 3 || lastAction == 4) && (tempAction == 3 || tempAction == 4) && k <= 10)) {
+            while ((lastAction == tempAction && k <= 10) || ((lastAction == 4) && (tempAction == 4) && k <= 10)) {
                 tempAction = Actions[getRandomIntInclusive(1, Actions.length) - 1];
                 k++;
             }
@@ -258,7 +273,7 @@ function ExamplesGeneration() {
                 }
             }
             if (lastAction == 4) {
-                tempNumber = dividerFind(lastNumber);
+                tempNumber = dividerFind(Math.abs(lastNumber));
             }
 
             lastNumber = tempNumber;
@@ -267,10 +282,8 @@ function ExamplesGeneration() {
             if (tempNumber < 0) {
                 tempNumber = '(' + tempNumber + ')';
             }
-            console.log(tempExample, tempNumber, lastAction);
             tempExampleMath += (String(tempNumber) + ' ' + getStringActionMath(tempAction) + ' ');
             tempExample += (String(tempNumber) + ' ' + getStringAction(tempAction) + ' ');
-            console.log(tempExample);
         }
 
         tempNumber = getRandomIntInclusive(FirstNumber, LastNumber);
@@ -298,7 +311,7 @@ function ExamplesGeneration() {
             }
         }
         if (lastAction == 4) {
-            tempNumber = dividerFind(lastNumber);
+            tempNumber = dividerFind(Math.abs(lastNumber));
         }
         if (tempNumber < 0) {
             tempExample += '(' + tempNumber + ')';
@@ -307,13 +320,10 @@ function ExamplesGeneration() {
             tempExample += tempNumber;
             tempExampleMath += tempNumber;
         }
-        
-        console.log(tempExample);
-        console.log(Examples.length);
+
         Examples.push(tempExample);
         Answers.push(eval(tempExampleMath));
-        
-        // document.getElementById('Example').innerText = (tempExample + ' = ');
+
     }
 }
 
@@ -365,19 +375,25 @@ function getStringActionMath(action) {
             break;
     }
 }
+
 function dividerFind(n) {
     let dividers = [];
-    for (let i = FirstNumber; i <= LastNumber; i++) {
+    for (let i = 1; i <= n; i++) {
         if (n % i == 0) {
             dividers.push(i);
         }
     }
-    let temp = dividers[getRandomIntInclusive(1, dividers.length) - 1];
-    let k = 1;
-    while ((temp == 0 || temp == 1 || temp == -1 || temp == n) && (k <= 10)) {
-        temp = dividers[getRandomIntInclusive(1, dividers.length) - 1];
-        k += 1
+    if (dividers == []) {
+        return n;
+    } else {
+        let temp = dividers[getRandomIntInclusive(1, dividers.length) - 1];
+        let k = 1;
+        while ((temp == 0 || temp == 1 || temp == -1 || temp == n) && (k <= 10)) {
+            temp = dividers[getRandomIntInclusive(1, dividers.length) - 1];
+            k += 1
+        }
+        return (temp);
     }
-    return (temp);
+
 }
 // Генерация примеров
